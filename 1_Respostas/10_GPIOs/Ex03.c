57075043bc7f6f98c346e6b6ceb7690613c15b7f
rFeijoo <goldcard99@hotmail.com>
2020-10-29 01:03:01 -0300

    Added 10_GPIOs

--- 1_Respostas/10_GPIOs/Ex03.c
+++ 1_Respostas/10_GPIOs/Ex03.c
@@ -3,128 +3,120 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 #include "gpio_sysfs.h"
 
-// const int led_pin = 4;
-// const int btn_pin = 4;//17;
+const int led_pin = 4;
+const int btn_pin = 17;
 
-// void parent_process(int *fd)
-// {
-//   int state;
-//   char msg[30];
-
-//   setup_io();
-
-//   INP_GPIO(btn_pin);
-
-//   // printf("ESTADO = %d\n", GET_GPIO(btn_pin));
-
-//   while (1)
-//   {
-//     puts("Pressione o botao");
-//     while (GET_GPIO(btn_pin));
-//     puts("Botao pressionado");
-//     while (GET_GPIO(btn_pin)==0);
-//     puts("Botao solto");
-//   }
-
-//   // for (int i=1; i<=10; i++)
-//   // {
-//   //   sprintf(msg, "%d", i);
-
-//   //   if (write(fd[1], msg, sizeof(msg)) < 0)
-//   //   {
-//   //     printf("Erro na escrita da mensagem\n");
-//   //     wait(&state);
-//   //   }
-
-//   //   sleep(1);
-//   // }
-
-//   // printf("Fim de execução do processo pai [PID = %d]\n", (int)getpid());
-
-//   exit(1);
-
-// }
-
-// void son_process(int *fd)
-// {
-//   char msg[30];
-//   int n = 0;
-
-//   // while (n != 10)
-//   // {
-//   //   if (read(fd[0], msg, 30) < 0)
-//   //     printf("Erro na leitura da mensagem\n");
-//   //   else
-//   //     printf("Mensagem recebida [PID = %d] = %s\n", (int)getpid(), msg);
-
-//   //   n = atoi(msg);
-//   // }
-
-//   printf("Fim de execução do processo filho [PID = %d]\n", (int)getpid());
-
-//   exit(1);
-// }
-
-// // int main(int argc, char **argv)
-// // {
-// //   setup_io();
-// //   INP_GPIO(4);
-// //   puts("Pressione o botao");
-// //   while (GET_GPIO(4));
-// //   return 0;
-
-
-
-// //   // int pid, fd[2];
-
-// //   // printf("Processo principal [PID = %d]\n", (int)getpid());
-
-// //   // pipe(fd);
-
-// //   // pid = fork();
-
-// //   // if (pid == 0)
-// //   //   son_process(fd);
-// //   // else
-// //   // if (pid != 0)
-// //   //   parent_process(fd);
-
-// //   return 0;
-// // }
-
-
-
-
-int pin = 4;
+int son_pid;
 
 void unset_GPIO(int signum)
 {
-    fprintf(stderr, "\nGPIO unset\n");
+  kill(son_pid, SIGINT);
+  fprintf(stderr, "\nSon process killed.\n");
 
-    if (unsetGPIO(pin) < 0)
-        exit(-3);
+  unsetGPIO(led_pin);
+  fprintf(stderr, "LED GPIO free\n");
 
-    exit(0);
+  unsetGPIO(btn_pin);
+  fprintf(stderr, "BTN GPIO free\n");
+
+  exit(0);
+}
+
+void parent_process(int *fd)
+{
+  int state;
+  char msg[30];
+
+  int last_freq, freq = 500000;
+
+  signal(SIGINT, unset_GPIO);
+
+  if (setGPIOdirection(btn_pin, "in") < 0)
+  {
+    printf("ERROR - unable to set GPIO %d\n", btn_pin);
+      
+    unset_GPIO(0);
+  }
+
+  last_freq = freq;
+
+  for (;;)
+  {
+    if (GPIOread(btn_pin))
+    {
+      freq /= 2;
+
+      sleep(1); // debounce
+    }
+
+    if (freq < 7812)
+      freq = 500000;
+
+    if (freq != last_freq)
+    {
+      sprintf(msg, "%d", freq);
+
+      if (write(fd[1], msg, sizeof(msg)) < 0)
+      {
+        printf("ERROR - failed to send message to son\n");
+
+        unset_GPIO(0);
+      }
+
+      last_freq = freq;
+    }
+  }
+}
+
+void son_process(int *fd)
+{
+  char msg[30];
+  int n = 0;
+
+  int freq = 500000;
+
+  if (setGPIOdirection(led_pin, "out") < 0)
+  {
+    printf("ERROR - unable to set GPIO %d\n", led_pin);
+      
+    kill(getppid(), SIGINT);
+  }
+
+  for (;;)
+  {
+    if (read(fd[0], msg, 30) > 0)
+      freq = atoi(msg);
+
+    GPIOwrite(led_pin, 1);
+
+    usleep(freq);
+
+    GPIOwrite(led_pin, 0);
+
+    usleep(freq);
+  }
 }
 
 int main(int argc, char **argv)
 {
-    signal(SIGINT, unset_GPIO);
+  int fd[2];
 
-    if (setGPIOdirection(pin, "in") < 0)
-    {
-        printf("ERROR - unable to set GPIO %d\n", pin);
-        return -1;
-    }
+  printf("Processo principal [PID = %d]\n", (int)getpid());
 
-    for (;;)
-    {
-        printf("STATE = %d\n", GPIOread(pin));
+  pipe(fd);
 
-        sleep(1);
-    }
+  if (fcntl(fd[0], F_SETFL, O_NONBLOCK) < 0) 
+    exit(1);
 
-    return 0;
+  son_pid = fork();
+
+  if (son_pid == 0)
+    son_process(fd);
+  else
+    parent_process(fd);
+
+  return 0;
 }
