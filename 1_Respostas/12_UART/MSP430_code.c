2e1ce9059aaf730251b7cdc7ffa241fb180a8512
Rafael Feijó Leonardo <goldcard99@hotmail.com>
2020-10-31 14:20:23 -0300

    Adding 12_UART

---
+++ 1_Respostas/12_UART/MSP430_code.c
@@ -0,0 +1,102 @@
+#include <msp430g2553.h>
+#include <legacymsp430.h>
+
+#define TX    BIT1
+#define RX    BIT2
+#define BTN   BIT3
+#define LED1  BIT0
+#define LED2  BIT6
+#define LEDS  (LED1|LED2)
+#define DLY1  0x6000
+#define DLY2  0x3000
+
+void delay(volatile unsigned int x)
+{
+	while (x--)
+    ;
+}
+
+void Send_Data(volatile unsigned char c)
+{
+	while ((IFG2 & UCA0TXIFG) == 0)
+    ;
+
+	UCA0TXBUF = c;
+}
+
+void ft_blink(unsigned char pin, char ft_blinkdas, unsigned int t)
+{
+	ft_blinkdas <<= 1;
+
+	while (ft_blinkdas--)
+	{
+		P1OUT ^= pin;
+
+		delay(t);
+	}
+}
+
+int main(void)
+{
+	WDTCTL = WDTPW + WDTHOLD;
+
+  BCSCTL1 = CALBC1_1MHZ;
+	DCOCTL  = CALDCO_1MHZ;
+
+  P1SEL2 = P1SEL = RX + TX;
+
+	P1DIR |= LEDS;
+	P1OUT &= ~LEDS;
+	P1DIR &= ~BTN;
+
+	P1REN |= BTN;
+	P1OUT |= BTN;
+
+	P1IES = 0;
+	P1IE  = BTN;
+
+	UCA0CTL0 = 0;
+	UCA0CTL1 = UCSSEL_2;
+	UCA0BR0  = 6;
+	UCA0BR1  = 0;
+	UCA0MCTL = UCBRF_8 + UCOS16;
+
+	IE2 |= UCA0RXIE;
+
+	ft_blink(LEDS, 1, DLY1);
+
+	_BIS_SR(LPM0_bits + GIE);
+
+	return 0;
+}
+
+interrupt(PORT1_VECTOR) P1_ISR(void)
+{
+	static unsigned char count = 0;
+
+	ft_blink(LED1, count, DLY1);
+
+	Send_Data(count);
+
+	count = (count == 5) ? 0:(count + 1);
+
+	P1IFG &= ~BTN;
+}
+
+interrupt(USCIAB0RX_VECTOR) Receive_Data(void)
+{
+	volatile unsigned char c = UCA0RXBUF;
+
+	P1OUT ^= LED1;
+
+	if (blink < 6)
+	{
+		ft_blink(LED2, c, DLY1);
+
+		Send_Data(c);
+	}
+
+	IFG2 &= ~UCA0RXIFG;
+
+	P1OUT ^= LED1;
+}
