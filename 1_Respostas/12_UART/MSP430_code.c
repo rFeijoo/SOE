db3d631abd84f96bd8660952fbc256c92441dfd1
rFeijoo <goldcard99@hotmail.com>
2020-10-31 21:37:33 -0300

    Update 12_UART

--- 1_Respostas/12_UART/MSP430_code.c
+++ 1_Respostas/12_UART/MSP430_code.c
@@ -1,5 +1,4 @@
 #include <msp430g2553.h>
-#include <legacymsp430.h>
 
 #define TX    BIT1
 #define RX    BIT2
@@ -12,91 +11,93 @@
 
 void delay(volatile unsigned int x)
 {
-	while (x--)
+    while (x--)
     ;
 }
 
 void Send_Data(volatile unsigned char c)
 {
-	while ((IFG2 & UCA0TXIFG) == 0)
+    while ((IFG2 & UCA0TXIFG) == 0)
     ;
 
-	UCA0TXBUF = c;
+    UCA0TXBUF = c;
 }
 
 void ft_blink(unsigned char pin, char ft_blinkdas, unsigned int t)
 {
-	ft_blinkdas <<= 1;
+    ft_blinkdas <<= 1;
 
-	while (ft_blinkdas--)
-	{
-		P1OUT ^= pin;
+    while (ft_blinkdas--)
+    {
+        P1OUT ^= pin;
 
-		delay(t);
-	}
+        delay(t);
+    }
 }
 
 int main(void)
 {
-	WDTCTL = WDTPW + WDTHOLD;
+    WDTCTL = WDTPW + WDTHOLD;
 
   BCSCTL1 = CALBC1_1MHZ;
-	DCOCTL  = CALDCO_1MHZ;
+    DCOCTL  = CALDCO_1MHZ;
 
   P1SEL2 = P1SEL = RX + TX;
 
-	P1DIR |= LEDS;
-	P1OUT &= ~LEDS;
-	P1DIR &= ~BTN;
+    P1DIR |= LEDS;
+    P1OUT &= ~LEDS;
+    P1DIR &= ~BTN;
 
-	P1REN |= BTN;
-	P1OUT |= BTN;
+    P1REN |= BTN;
+    P1OUT |= BTN;
 
-	P1IES = 0;
-	P1IE  = BTN;
+    P1IES = 0;
+    P1IE  = BTN;
 
-	UCA0CTL0 = 0;
-	UCA0CTL1 = UCSSEL_2;
-	UCA0BR0  = 6;
-	UCA0BR1  = 0;
-	UCA0MCTL = UCBRF_8 + UCOS16;
+    UCA0CTL0 = 0;
+    UCA0CTL1 = UCSSEL_2;
+    UCA0BR0  = 6;
+    UCA0BR1  = 0;
+    UCA0MCTL = UCBRF_8 + UCOS16;
 
-	IE2 |= UCA0RXIE;
+    IE2 |= UCA0RXIE;
 
-	ft_blink(LEDS, 1, DLY1);
+    ft_blink(LEDS, 1, DLY1);
 
-	_BIS_SR(LPM0_bits + GIE);
+    _BIS_SR(LPM0_bits + GIE);
 
-	return 0;
+    return 0;
 }
 
-interrupt(PORT1_VECTOR) P1_ISR(void)
+#pragma vector = PORT1_VECTOR
+__interrupt void P1_ISR (void)
 {
-	static unsigned char count = 0;
+    static unsigned char count = 0;
 
-	ft_blink(LED1, count, DLY1);
+    ft_blink(LED1, count, DLY1);
 
-	Send_Data(count);
+    Send_Data(count);
 
-	count = (count == 5) ? 0:(count + 1);
+    count = (count == 5) ? 0:(count + 1);
 
-	P1IFG &= ~BTN;
+    P1IFG &= ~BTN;
 }
 
-interrupt(USCIAB0RX_VECTOR) Receive_Data(void)
+#pragma vector = USCIAB0RX_VECTOR
+__interrupt void Receive_Data (void)
 {
-	volatile unsigned char c = UCA0RXBUF;
+    volatile unsigned char c = UCA0RXBUF;
 
-	P1OUT ^= LED1;
+    P1OUT ^= LED1;
 
-	if (blink < 6)
-	{
-		ft_blink(LED2, c, DLY1);
+    if (c < 6)
+    {
+        ft_blink(LED2, c, DLY1);
 
-		Send_Data(c);
-	}
+        Send_Data(c);
+    }
 
-	IFG2 &= ~UCA0RXIFG;
+    IFG2 &= ~UCA0RXIFG;
 
-	P1OUT ^= LED1;
+    P1OUT ^= LED1;
 }
---
+++ 1_Respostas/12_UART/example.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Para RPi 0, 1 e 2
+# UART=/dev/ttyAMA0
+# Para RPi 0W, 3 e 4
+UART=/dev/ttyS0
+
+echo
+echo
+echo
+
+# Configure a porta serial
+stty -F $UART ispeed 9600 ospeed 9600 cs8 -cstopb -parenb
+# Confira o resultado da configuracao
+stty -F $UART -a
+
+echo
+echo
+echo
+
+echo Pressione ENTER para ver o LED do MSP430 piscar 1 vez
+read
+echo -n -e \\x01 >> $UART
+echo Pressione ENTER para ver o LED do MSP430 piscar 3 vezes
+read
+echo -n -e \\x03 >> $UART
